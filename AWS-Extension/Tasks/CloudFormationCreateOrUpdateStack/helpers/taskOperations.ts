010a24c5f92f15ab01aeab39fde35ba9a75c44dd
Steve Roberts <strobe@amazon.com>
2017-07-18 13:36:26 -0700

    Collapsed create/update stack and create change set tasks together for simplicity.

--- AWS-Extension/Tasks/CloudFormationCreateOrUpdateStack/helpers/taskOperations.ts
+++ AWS-Extension/Tasks/CloudFormationCreateOrUpdateStack/helpers/taskOperations.ts
@@ -15,10 +15,10 @@ export class TaskOperations {
         let stackId: string = await this.testStackExists(taskParameters.stackName);
         if (stackId) {
             console.log(tl.loc('StackExists', taskParameters.stackName));
-            await this.updateStackFromTemplate(taskParameters);
+            await this.updateStack(taskParameters);
         } else {
             console.log(tl.loc('StackNotExist', taskParameters.stackName));
-            stackId = await this.createStackFromTemplate(taskParameters);
+            stackId = await this.createStack(taskParameters);
         }
 
         if (taskParameters.outputVariable) {
@@ -70,8 +70,7 @@ export class TaskOperations {
         return null;
     }
 
-    private static async updateStackFromTemplate(taskParameters: TaskParameters.CreateOrUpdateStackTaskParameters) : Promise<void> {
-        console.log(tl.loc('UpdateStackFromTemplate', taskParameters.templateFile, taskParameters.templateParametersFile));
+    private static async createStack(taskParameters: TaskParameters.CreateOrUpdateStackTaskParameters) : Promise<string> {
 
         let template: string;
         let templateParameters: awsCloudFormation.Parameters;
@@ -86,57 +85,141 @@ export class TaskOperations {
             throw err;
         }
 
-        const request: awsCloudFormation.UpdateStackInput = {
-            StackName: taskParameters.stackName,
-            Parameters: templateParameters,
-            TemplateBody: template
-        };
-        try {
-            const response: awsCloudFormation.UpdateStackOutput = await this.cloudFormationClient.updateStack(request).promise();
-            await this.waitForStackUpdate(request.StackName);
-        } catch (err) {
-            // if there were no changes, a validation error is thrown which we want to suppress
-            // rather than erroring out and failing the build
-            if (err.code.search(/ValidationError/) !== -1 && err.message.search(/^No updates are to be performed./) !== -1) {
-                tl.warning(tl.loc('NoWorkToDo'));
-                return;
+        if (taskParameters.useChangeset) {
+            console.log(tl.loc('CreateStackWithChangeset', taskParameters.templateFile, taskParameters.templateParametersFile));
+            return await this.createOrUpdateWithChangeset(taskParameters, 'CREATE', template, templateParameters);
+        } else {
+            console.log(tl.loc('CreateStack', taskParameters.templateFile, taskParameters.templateParametersFile));
+
+            const request: awsCloudFormation.CreateStackInput = {
+                StackName: taskParameters.stackName,
+                OnFailure: taskParameters.onFailure,
+                Parameters: templateParameters,
+                TemplateBody: template,
+                RoleARN: taskParameters.roleARN
+            };
+
+            if (taskParameters.notificationARNs && taskParameters.notificationARNs.length > 0) {
+                request.NotificationARNs = taskParameters.notificationARNs;
+            }
+            if (taskParameters.resourceTypes && taskParameters.resourceTypes.length > 0) {
+                request.ResourceTypes = taskParameters.resourceTypes;
             }
 
-            console.error(tl.loc('StackUpdateRequestFailed', err.message), err);
+            try {
+                const response: awsCloudFormation.CreateStackOutput = await this.cloudFormationClient.createStack(request).promise();
+                tl.debug(`Stack id ${response.StackId}`);
+                await this.waitForStackCreation(request.StackName);
+                return response.StackId;
+            } catch (err) {
+                console.error(tl.loc('StackCreateRequestFailed', err.message), err);
+                throw err;
+            }
+        }
+    }
+
+    private static async updateStack(taskParameters: TaskParameters.CreateOrUpdateStackTaskParameters) : Promise<void> {
+        console.log(tl.loc('UpdateStack', taskParameters.templateFile, taskParameters.templateParametersFile));
+
+        let template: string;
+        let templateParameters: awsCloudFormation.Parameters;
+
+        try {
+            template = await this.loadTemplateFile(taskParameters.templateFile);
+            if (taskParameters.templateParametersFile) {
+                templateParameters = await this.loadParametersFromFile(taskParameters.templateParametersFile);
+            }
+        } catch (err) {
+            console.error(tl.loc('TemplateFilesLoadFailure', err.message), err);
+            throw err;
+        }
+
+        if (taskParameters.useChangeset) {
+            console.log(tl.loc('UpdateStackWithChangeset', taskParameters.templateFile, taskParameters.templateParametersFile));
+            await this.createOrUpdateWithChangeset(taskParameters, 'UPDATE', template, templateParameters);
+        } else {
+            const request: awsCloudFormation.UpdateStackInput = {
+                StackName: taskParameters.stackName,
+                Parameters: templateParameters,
+                TemplateBody: template,
+                RoleARN: taskParameters.roleARN
+            };
+
+            if (taskParameters.notificationARNs && taskParameters.notificationARNs.length > 0) {
+                request.NotificationARNs = taskParameters.notificationARNs;
+            }
+            if (taskParameters.resourceTypes && taskParameters.resourceTypes.length > 0) {
+                request.ResourceTypes = taskParameters.resourceTypes;
+            }
+
+            try {
+                const response: awsCloudFormation.UpdateStackOutput = await this.cloudFormationClient.updateStack(request).promise();
+                await this.waitForStackUpdate(request.StackName);
+            } catch (err) {
+                // if there were no changes, a validation error is thrown which we want to suppress
+                // rather than erroring out and failing the build
+                if (err.code.search(/ValidationError/) !== -1 && err.message.search(/^No updates are to be performed./) !== -1) {
+                    tl.warning(tl.loc('NoWorkToDo'));
+                    return;
+                }
+
+                console.error(tl.loc('StackUpdateRequestFailed', err.message), err);
+                throw err;
+            }
+        }
+    }
+
+    private static async createOrUpdateWithChangeset(taskParameters: TaskParameters.CreateOrUpdateStackTaskParameters,
+                                                     changesetType: string,
+                                                     templateBody: string,
+                                                     templateParameters: awsCloudFormation.Parameters) : Promise<string> {
+
+        const request: awsCloudFormation.CreateChangeSetInput = {
+            ChangeSetName: taskParameters.changesetName,
+            ChangeSetType: changesetType,
+            StackName: taskParameters.stackName,
+            Parameters: templateParameters,
+            TemplateBody: templateBody,
+            Description: taskParameters.description,
+            RoleARN: taskParameters.roleARN
+        };
+
+        if (taskParameters.notificationARNs && taskParameters.notificationARNs.length > 0) {
+            request.NotificationARNs = taskParameters.notificationARNs;
+        }
+        if (taskParameters.resourceTypes && taskParameters.resourceTypes.length > 0) {
+            request.ResourceTypes = taskParameters.resourceTypes;
+        }
+
+        try {
+            const response: awsCloudFormation.CreateChangeSetOutput = await this.cloudFormationClient.createChangeSet(request).promise();
+
+            tl.debug(`Change set id ${response.Id}, stack id ${response.StackId}`);
+            await this.waitForChangeSetCreation(request.ChangeSetName, response.StackId);
+
+            if (taskParameters.autoExecute) {
+                await this.executeChangeSet(taskParameters.changesetName, taskParameters.stackName);
+            }
+
+            return response.StackId;
+        }  catch (err) {
+            console.error(tl.loc('ChangesetCreationFailed', err.message), err);
             throw err;
         }
     }
 
-    private static async createStackFromTemplate(taskParameters: TaskParameters.CreateOrUpdateStackTaskParameters) : Promise<string> {
-
-        console.log(tl.loc('CreateStackFromTemplate', taskParameters.templateFile, taskParameters.templateParametersFile));
-
-        let template: string;
-        let templateParameters: awsCloudFormation.Parameters;
+    private static async executeChangeSet(changeSetName: string, stackName: string) : Promise<void> {
+        console.log(tl.loc('ExecutingChangeset', changeSetName, stackName));
 
         try {
-            template = await this.loadTemplateFile(taskParameters.templateFile);
-            if (taskParameters.templateParametersFile) {
-                templateParameters = await this.loadParametersFromFile(taskParameters.templateParametersFile);
-            }
-        } catch (err) {
-            console.error(tl.loc('TemplateFilesLoadFailure', err.message), err);
-            throw err;
-        }
+            await this.cloudFormationClient.executeChangeSet({
+                ChangeSetName: changeSetName,
+                StackName: stackName
+            }).promise();
 
-        const request: awsCloudFormation.CreateStackInput = {
-            StackName: taskParameters.stackName,
-            OnFailure: taskParameters.onFailure,
-            Parameters: templateParameters,
-            TemplateBody: template
-        };
-        try {
-            const response: awsCloudFormation.CreateStackOutput = await this.cloudFormationClient.createStack(request).promise();
-            tl.debug(`Stack id ${response.StackId}`);
-            await this.waitForStackCreation(request.StackName);
-            return response.StackId;
+            await this.waitForStackCreation(stackName);
         } catch (err) {
-            console.error(tl.loc('StackCreateRequestFailed', err.message), err);
+            console.error(tl.loc('ExecuteChangesetFailed', err.message), err);
             throw err;
         }
     }
@@ -184,6 +267,23 @@ export class TaskOperations {
                     throw new Error(tl.loc('StackUpdateFailed', stackName, err.message));
                 } else {
                     console.log(tl.loc('StackUpdated', stackName));
+                }
+            });
+        });
+    }
+
+    private static async waitForChangeSetCreation(changeSetName: string, stackId: string) : Promise<void> {
+
+        return new Promise<void>((resolve, reject) => {
+            console.log(tl.loc('WaitingForChangesetValidation', changeSetName));
+
+            this.cloudFormationClient.waitFor('changeSetCreateComplete',
+                                              { ChangeSetName: changeSetName, StackName: stackId },
+                                              function(err: AWSError, data: awsCloudFormation.DescribeChangeSetOutput) {
+                if (err) {
+                    throw new Error(tl.loc('ChangesetValidationFailed', changeSetName, err.message));
+                } else {
+                    console.log(tl.loc('ChangesetValidated'));
                 }
             });
         });
--- AWS-Extension/Tasks/CloudFormationCreateOrUpdateStack/helpers/taskParameters.ts
+++ AWS-Extension/Tasks/CloudFormationCreateOrUpdateStack/helpers/taskParameters.ts
@@ -3,10 +3,18 @@ import tl = require('vsts-task-lib/task');
 export class CreateOrUpdateStackTaskParameters {
     public awsKeyId: string;
     public awsSecretKey: string;
-    public stackName: string;
     public awsRegion: string;
+    public stackName: string;
     public templateFile: string;
     public templateParametersFile: string;
+    public useChangeset: boolean;
+    public changesetName: string;
+    public description: string;
+    public autoExecute: boolean;
+    public roleARN: string;
+    public notificationARNs: string[];
+    public resourceTypes: string[];
+
     public onFailure: string;
     public outputVariable: string;
 
@@ -18,8 +26,19 @@ export class CreateOrUpdateStackTaskParameters {
             this.awsSecretKey = awsEndpointAuth.parameters.password;
             this.awsRegion = tl.getInput('regionName', true);
             this.stackName = tl.getInput('stackName', true);
+
             this.templateFile = tl.getPathInput('templateFile', true, true);
-            this.templateParametersFile = tl.getPathInput('templateParametersFile', true, true);
+            this.templateParametersFile = tl.getPathInput('templateParametersFile', false, true);
+
+            this.useChangeset = tl.getBoolInput('useChangeset', false);
+            this.changesetName = tl.getInput('changesetName', this.useChangeset);
+            this.description = tl.getInput('description', false);
+            this.autoExecute = tl.getBoolInput('autoExecute', false);
+
+            this.roleARN = tl.getInput('roleARN', false);
+            this.notificationARNs = tl.getDelimitedInput('notificationARNs', '\n', false);
+            this.resourceTypes = tl.getDelimitedInput('resourceTypes', '\n', false);
+
             this.onFailure = tl.getInput('onFailure');
             this.outputVariable = tl.getInput('outputVariable', false);
         } catch (error) {
