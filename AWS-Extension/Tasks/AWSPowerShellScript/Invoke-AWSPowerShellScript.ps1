cec76b31c5be2f0d428eda7d85310240d1624470
Steve Roberts <strobe@amazon.com>
2017-07-25 14:49:46 -0700

    Version bump for new test release

--- AWS-Extension/Tasks/AWSPowerShellScript/Invoke-AWSPowerShellScript.ps1
+++ AWS-Extension/Tasks/AWSPowerShellScript/Invoke-AWSPowerShellScript.ps1
@@ -1,5 +1,6 @@
 Trace-VstsEnteringInvocation $MyInvocation
 Import-VstsLocStrings "$PSScriptRoot\Task.json"
+
 function Test-AWSPowerShellModuleInstalled($installIfRequired)
 {
     Write-Host (Get-VstsLocString -Key "VerifyingAWSPowerShellModuleInstalled")
@@ -10,13 +11,8 @@ function Test-AWSPowerShellModuleInstalled($installIfRequired)
         if ($installIfRequired)
         {
             Write-Host (Get-VstsLocString -Key "InstallingModule")
-            $installBlockString =
-@"
-Install-PackageProvider -Name NuGet -Force -Scope CurrentUser -Verbose
-Install-Module -Name AWSPowerShell -Force -Scope CurrentUser -Verbose
-"@
-            $installBlock = [Scriptblock]::Create($installBlockString)
-            Invoke-Command -ScriptBlock $installBlock
+            Install-PackageProvider -Name NuGet -Force -Scope CurrentUser -Verbose
+            Install-Module -Name AWSPowerShell -Force -Scope CurrentUser -Verbose
         }
         else
         {
@@ -47,11 +43,6 @@ try
     }
     Initialize-AWSDefaultConfiguration @initParams
 
-    # Run the user's command or script. If the user gave us a file, load it
-    # into a script block. Prefer this over serializing a script block to
-    # disk in case the script contains sensitive material that the user
-    # might not expect to be left on the file system (should an error
-    # cause clean up failure)
     if ($scriptType -eq 'filePath')
     {
         $scriptFile = Get-VstsInput -Name 'scriptName' -Require
@@ -66,7 +57,7 @@ try
 
     $scriptblock = [Scriptblock]::Create($script)
     Write-Host (Get-VstsLocString -Key "ExecutingScript")
-    Invoke-Command -ScriptBlock $scriptblock -ArgumentList $arguments -Verbose
+    $scriptblock.Invoke($arguments)
 }
 finally
 {
