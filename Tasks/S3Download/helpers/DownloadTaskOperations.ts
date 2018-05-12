a40e1eb9776d10b14b626a783f20d01ff253c2c8
Steve Roberts <strobe@amazon.com>
2018-05-11 17:44:48 -0700

    Copyright banner update

--- Tasks/S3Download/helpers/DownloadTaskOperations.ts
+++ Tasks/S3Download/helpers/DownloadTaskOperations.ts
@@ -1,5 +1,5 @@
 /*
-  * Copyright 2017 Amazon.com, Inc. and its affiliates. All Rights Reserved.
+  Copyright 2017-2018 Amazon.com, Inc. and its affiliates. All Rights Reserved.
   *
   * Licensed under the MIT License. See the LICENSE accompanying this file
   * for the specific language governing permissions and limitations under
