3ac332c647a5ebe17f32956b8c661e9ee35f387c
Hunter Werlla <werlla@amazon.com>
2019-05-03 13:33:01 -0700

    add comment

--- Tasks/Common/sdkutils/awsConnectionParameters.ts
+++ Tasks/Common/sdkutils/awsConnectionParameters.ts
@@ -1,1 +1,3 @@
+// TODO TODO TODO this is a HACK!
+// see this github issue for tracking removal of this https://github.com/aws/aws-vsts-tools/issues/166
 export { AWSTaskParametersBase as AWSConnectionParameters } from './awsTaskParametersBase';
