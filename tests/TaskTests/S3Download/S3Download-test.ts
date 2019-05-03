0408173dd754fa8457d387a2d33296c45cf0070f
Hunter Werlla <werlla@amazon.com>
2019-05-03 16:41:39 -0700

    start working on tests

--- tests/TaskTests/S3Download/S3Download-test.ts
+++ tests/TaskTests/S3Download/S3Download-test.ts
@@ -2,9 +2,11 @@ import { TaskOperations } from '../../../Tasks/S3Download/DownloadTaskOperations
 import { TaskParameters } from '../../../Tasks/S3Download/DownloadTaskParameters';
 import { SdkUtils } from '../../../Tasks/Common/sdkutils/sdkutils';
 import path = require('path');
+const fs = require('fs');
 const AWS = require('aws-sdk');
 
 describe('S3 Download', () => {
+    // TODO https://github.com/aws/aws-vsts-tools/issues/167
     beforeAll(() => {
         SdkUtils.readResourcesFromRelativePath('../../../Tasks/S3Download/task.json');
     });
@@ -21,5 +23,28 @@ describe('S3 Download', () => {
         const taskOperation = new TaskOperations(s3, taskParameters);
         expect.assertions(1);
         await taskOperation.execute().catch((e) => { expect(e.message).toContain('not exist'); });
+    });
+
+    test('Makes a folder if it does not exist', async () => {
+        const targetFolder: string = './folder';
+        fs.rmdir(targetFolder, (err) => {});
+        const s3 = new AWS.S3({region: 'us-east-1'});
+        s3.headBucket = jest.fn((params, cb) => { });
+        const taskParameters = new TaskParameters();
+        taskParameters.targetFolder = targetFolder;
+        const taskOperation = new TaskOperations(s3, taskParameters);
+        await taskOperation.execute().catch((e) => { /* ignored as this is not required for the test */});
+        fs.access(targetFolder, fs.constants.F_OK, (err) => {
+            throw new Error("Failed to create file!")
+        });
+        console.log('successfully created folder');
+    });
+
+    test('Constructs a proper S3 connection string', async () => {
+        const s3 = new AWS.S3({region: 'us-east-1'});
+        s3.headBucket = jest.fn((params, cb) => { });
+        const taskParameters = new TaskParameters();
+        const taskOperation = new TaskOperations(s3, taskParameters);
+        // TODO
     });
 });
