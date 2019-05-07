c7f1cb46d6f7337b078e1e808911d7e1dffe0d79
Hunter Werlla <werlla@amazon.com>
2019-05-07 15:07:53 -0700

    generates strings now too

--- build-scripts/generateResources.js
+++ build-scripts/generateResources.js
@@ -5,6 +5,7 @@
 
 const fs = require('fs-extra')
 const path = require('path')
+var validate = require('validator')
 
 const taskJson = 'task.json'
 const TaskLocJson = 'task.loc.json'
@@ -17,11 +18,25 @@ function findMatchingFiles(directory) {
     return fs.readdirSync(directory)
 }
 
-function createTaskLocJson(taskPath) {
-    var taskJsonPath = path.join(inTasks, taskPath, taskJson);
-    try {fs.accessSync(taskJsonPath) } catch (e) { return }
+var validateTask = function (task) {
+    if (!task.id || !validate.isUUID(task.id)) {
+        fail('id is a required guid');
+    };
 
-    var taskLoc = JSON.parse(fs.readFileSync(taskJsonPath));
+    if (!task.name || !validate.isAlphanumeric(task.name)) {
+        fail('name is a required alphanumeric string');
+    }
+
+    if (!task.friendlyName || !validate.isLength(task.friendlyName, 1, 40)) {
+        fail('friendlyName is a required string <= 40 chars');
+    }
+
+    if (!task.instanceNameFormat) {
+        fail('instanceNameFormat is required');
+    }
+};
+
+function generateTaskLoc(taskLoc, taskPath) {
     taskLoc.friendlyName = 'ms-resource:loc.friendlyName';
     taskLoc.helpMarkDown = 'ms-resource:loc.helpMarkDown';
     taskLoc.description = 'ms-resource:loc.description';
@@ -58,6 +73,69 @@ function createTaskLocJson(taskPath) {
     }
 
     fs.writeFileSync(path.join(outTasks, taskPath, TaskLocJson), JSON.stringify(taskLoc, null, 2));
+}
+
+var createResjson = function (task, taskPath) {
+    var resources = {};
+    if (task.hasOwnProperty('friendlyName')) {
+        resources['loc.friendlyName'] = task.friendlyName;
+    }
+
+    if (task.hasOwnProperty('helpMarkDown')) {
+        resources['loc.helpMarkDown'] = task.helpMarkDown;
+    }
+
+    if (task.hasOwnProperty('description')) {
+        resources['loc.description'] = task.description;
+    }
+
+    if (task.hasOwnProperty('instanceNameFormat')) {
+        resources['loc.instanceNameFormat'] = task.instanceNameFormat;
+    }
+
+    if (task.hasOwnProperty('releaseNotes')) {
+        resources['loc.releaseNotes'] = task.releaseNotes;
+    }
+
+    if (task.hasOwnProperty('groups')) {
+        task.groups.forEach(function (group) {
+            if (group.hasOwnProperty('name')) {
+                resources['loc.group.displayName.' + group.name] = group.displayName;
+            }
+        });
+    }
+
+    if (task.hasOwnProperty('inputs')) {
+        task.inputs.forEach(function (input) {
+            if (input.hasOwnProperty('name')) {
+                resources['loc.input.label.' + input.name] = input.label;
+
+                if (input.hasOwnProperty('helpMarkDown') && input.helpMarkDown) {
+                    resources['loc.input.help.' + input.name] = input.helpMarkDown;
+                }
+            }
+        });
+    }
+
+    if (task.hasOwnProperty('messages')) {
+        Object.keys(task.messages).forEach(function (key) {
+            resources['loc.messages.' + key] = task.messages[key];
+        });
+    }
+
+    var resjsonPath = path.join(outTasks, taskPath, 'Strings', 'resources.resjson', 'en-US', 'resources.resjson');
+    mkdir('-p', path.dirname(resjsonPath));
+    fs.writeFileSync(resjsonPath, JSON.stringify(resources, null, 2));
+};
+
+function generateTaskResources(taskPath) {
+    var taskJsonPath = path.join(inTasks, taskPath, taskJson);
+    try {fs.accessSync(taskJsonPath) } catch (e) { return }
+
+    var task = JSON.parse(fs.readFileSync(taskJsonPath));
+    validateTask(task)
+    generateTaskLoc(task, taskPath)
+    createResjson(task, taskPath)
 };
 
 var createResjson = function (task, taskPath) {
@@ -108,32 +186,14 @@ var createResjson = function (task, taskPath) {
         });
     }
 
-    var resjsonPath = path.join(taskPath, 'Strings', 'resources.resjson', 'en-US', 'resources.resjson');
-    mkdir('-p', path.dirname(resjsonPath));
+    var resjsonPath = path.join(outTasks, taskPath, 'Strings', 'resources.resjson', 'en-US', 'resources.resjson');
+    fs.mkdirpSync(path.dirname(resjsonPath));
     fs.writeFileSync(resjsonPath, JSON.stringify(resources, null, 2));
 };
 
-// Validates the structure of a task.json file.
-var validateTask = function (task) {
-    if (!task.id || !check.isUUID(task.id)) {
-        fail('id is a required guid');
-    };
 
-    if (!task.name || !check.isAlphanumeric(task.name)) {
-        fail('name is a required alphanumeric string');
-    }
-
-    if (!task.friendlyName || !check.isLength(task.friendlyName, 1, 40)) {
-        fail('friendlyName is a required string <= 40 chars');
-    }
-
-    if (!task.instanceNameFormat) {
-        fail('instanceNameFormat is required');
-    }
-};
-
-findMatchingFiles().forEach((path) =>
+findMatchingFiles(inTasks).forEach((path) =>
     {
-        createTaskLocJson(path)
+        generateTaskResources(path)
     }
 )
