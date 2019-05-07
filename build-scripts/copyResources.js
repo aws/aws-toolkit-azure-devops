ae9cdcdcf088620eed8f3c4ea48670e8e2c3c8dc
Hunter Werlla <werlla@amazon.com>
2019-05-07 14:25:22 -0700

    starting to work

---
+++ build-scripts/copyResources.js
@@ -0,0 +1,44 @@
+/*!
+ * Copyright 2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: MIT
+ */
+
+/*
+    This script is called from npm run compile. It copies the
+    files and directories listed in `relativePaths` to `out/`.
+*/
+
+const fs = require('fs-extra')
+const path = require('path')
+
+const timeMessage = 'Copied resources'
+const tasksDirecotry = 'Tasks'
+const repoRoot = path.dirname(__dirname)
+const inTasks = path.join(repoRoot, tasksDirecotry)
+const outTasks = path.join(repoRoot, '_build', tasksDirecotry)
+
+const bannedFiles = [
+    'tsconfig.json',
+    'make.json'
+]
+
+const filterFunc = (src, dest) => {
+    return bannedFiles.every((element) => {
+        if(src.includes(element)) {
+            return false
+        }
+        return true
+    })
+}
+
+const options = {
+    overwrite: true,
+    filter: filterFunc
+}
+
+console.time(timeMessage)
+console.log('Copying files from ' + inTasks + ' to ' + outTasks)
+fs.copy(inTasks, outTasks, options)
+    .then(() => console.info('Successfully coppied files'))
+    .catch((error) => console.info('Copy failed with error: ' + error))
+console.timeEnd(timeMessage)
