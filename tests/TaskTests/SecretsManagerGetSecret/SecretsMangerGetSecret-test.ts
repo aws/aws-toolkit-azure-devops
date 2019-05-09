6996fee7d6ac083fd59d62d24ca1ab2b39403f7b
Hunter Werlla <werlla@amazon.com>
2019-05-09 16:27:34 -0700

    .

---
+++ tests/TaskTests/SecretsManagerGetSecret/SecretsMangerGetSecret-test.ts
@@ -0,0 +1,10 @@
+/*!
+ * Copyright 2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: MIT
+ */
+
+describe('Secrets Manger Get Secret', () => {
+    test('Does it', () => {
+        return true
+    })
+})
