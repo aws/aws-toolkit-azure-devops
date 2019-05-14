4252dfff675d3b76e5378fba92ca2aeddc929ec7
Hunter Werlla <werlla@amazon.com>
2019-05-14 14:58:22 -0700

    tests work

--- tests/TaskTests/SystemsManagerGetParameter/SystemsManagerGetParameter-test.ts
+++ tests/TaskTests/SystemsManagerGetParameter/SystemsManagerGetParameter-test.ts
@@ -13,43 +13,116 @@ import { TaskParameters } from '../../../Tasks/SystemsManagerGetParameter/TaskPa
 // tslint:disable: no-unsafe-any
 jest.mock('aws-sdk')
 
-describe('Systems Manager Get Parameter', () => {
-    const defaultTaskParameters: TaskParameters = {
-        awsConnectionParameters: undefined,
-        readMode: undefined,
-        parameterName: undefined,
-        parameterVersion: undefined,
-        parameterPath: undefined,
-        recursive: undefined,
-        variableNameTransform: undefined,
-        customVariableName: undefined,
-        replacementPattern: undefined,
-        replacementText: undefined,
-        globalMatch: undefined,
-        caseInsensitiveMatch: undefined
+const defaultTaskParameters: TaskParameters = {
+    awsConnectionParameters: undefined,
+    readMode: undefined,
+    parameterName: undefined,
+    parameterVersion: undefined,
+    parameterPath: undefined,
+    recursive: undefined,
+    variableNameTransform: undefined,
+    customVariableName: undefined,
+    replacementPattern: undefined,
+    replacementText: undefined,
+    globalMatch: undefined,
+    caseInsensitiveMatch: undefined
+}
+
+const singleParameterResponse = {
+    promise: function() {
+        return {
+            Parameter: {
+                Type: 'type',
+                Value: 'value'
+            }
+        }
     }
+}
+
+const hierarchyParameterResponse = {
+    promise: function() {
+        return {
+            Parameters: [{
+                Type: 'type',
+                Value: 'value'
+            }],
+            NextToken: undefined
+        }
+    }
+}
+
+const hierarchyParameterResponseMultiple = {
+    promise: function() {
+        return {
+            Parameters: [{
+                Type: 'type',
+                Value: 'value'
+            }],
+            NextToken: '23'
+        }
+    }
+}
+
+describe('Systems Manager Get Parameter', () => {
     // TODO https://github.com/aws/aws-vsts-tools/issues/167
     beforeAll(() => {
-        SdkUtils.readResourcesFromRelativePath('../../../_build/Tasks/SecretsManagerGetSecret/task.json')
+        SdkUtils.readResourcesFromRelativePath('../../../_build/Tasks/SystemsManagerGetParameter/task.json')
     })
 
     test('Creates a TaskOperation', () => {
         expect(new TaskOperations(new SSM(), defaultTaskParameters)).not.toBeNull()
     })
 
-    test('Read mode unknown throws', () => {
-
+    test('Read mode unknown throws', async () => {
+        expect.assertions(1)
+        const taskOperations = new TaskOperations(new SSM(), defaultTaskParameters)
+        taskOperations.execute().catch((e) => expect(e).toContain('is not a valid parameter'))
     })
 
-    test('Read mode single reads', () => {
-
+    test('Read mode single reads', async () => {
+        expect.assertions(1)
+        const taskParameters = {...defaultTaskParameters}
+        taskParameters.readMode = 'single'
+        taskParameters.variableNameTransform = 'none'
+        taskParameters.parameterName = 'yes'
+        const ssm = new SSM() as any
+        ssm.getParameter = jest.fn(() => singleParameterResponse)
+        const taskOperations = new TaskOperations(ssm, taskParameters)
+        await taskOperations.execute()
+        expect(ssm.getParameter).toBeCalled()
     })
 
-    test('Read mode hierarchy reads depth of one', () => {
-
+    test('Read mode hierarchy reads depth of one', async () => {
+        expect.assertions(1)
+        const taskParameters = {...defaultTaskParameters}
+        taskParameters.readMode = 'hierarchy'
+        taskParameters.variableNameTransform = 'none'
+        taskParameters.parameterName = 'yes'
+        taskParameters.parameterPath = '/params'
+        const ssm = new SSM() as any
+        ssm.getParametersByPath = jest.fn(() => hierarchyParameterResponse)
+        const taskOperations = new TaskOperations(ssm, taskParameters)
+        await taskOperations.execute()
+        expect(ssm.getParametersByPath).toBeCalledTimes(1)
     })
 
-    test('Read mode hierarchy reads recursively', () => {
-
+    test('Read mode hierarchy reads recursively', async () => {
+        expect.assertions(1)
+        const taskParameters = {...defaultTaskParameters}
+        taskParameters.readMode = 'hierarchy'
+        taskParameters.variableNameTransform = 'none'
+        taskParameters.parameterName = 'yes'
+        taskParameters.parameterPath = 'params'
+        const ssm = new SSM() as any
+        ssm.getParametersByPath = jest.fn((args) => {
+            if (args.NextToken === '23') {
+                return hierarchyParameterResponse
+            } else {
+                return hierarchyParameterResponseMultiple
+            }}
+        )
+        const taskOperations = new TaskOperations(ssm, taskParameters)
+        await taskOperations.execute()
+        expect(ssm.getParametersByPath).toBeCalledTimes(2)
     })
 })
