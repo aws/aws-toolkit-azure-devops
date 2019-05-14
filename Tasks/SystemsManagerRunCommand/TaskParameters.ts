683deee8e8af6705cc0a4b4800d4242129c46188
Hunter Werlla <werlla@amazon.com>
2019-05-14 16:16:23 -0700

    working on refactor

--- Tasks/SystemsManagerRunCommand/TaskParameters.ts
+++ Tasks/SystemsManagerRunCommand/TaskParameters.ts
@@ -3,77 +3,76 @@
  * SPDX-License-Identifier: MIT
  */
 
-import { AWSTaskParametersBase } from 'sdkutils/awsTaskParametersBase'
+import { AWSConnectionParameters, buildConnectionParameters } from 'Common/awsConnectionParameters'
 import tl = require('vsts-task-lib/task')
 
-export class TaskParameters extends AWSTaskParametersBase {
+export const fromInstanceIds: string = 'fromInstanceIds'
+export const fromTags: string = 'fromTags'
+export const fromBuildVariable: string = 'fromBuildVariable'
 
-    // instance selector options
-    public static readonly fromInstanceIds: string = 'fromInstanceIds'
-    public static readonly fromTags: string = 'fromTags'
-    public static readonly fromBuildVariable: string = 'fromBuildVariable'
+export interface TaskParameters {
+    awsConnectionParameters: AWSConnectionParameters,
+    documentName: string,
+    documentParameters: string,
+    serviceRoleARN: string,
+    comment: string,
+    instanceSelector: string,
+    instanceIds: string[],
+    instanceTags: string[],
+    instanceBuildVariable: string,
+    maxConcurrency: string,
+    maxErrors: string,
+    timeout: string,
+    notificationArn: string,
+    notificationEvents: string,
+    notificationType: string,
+    outputS3BucketName: string,
+    outputS3KeyPrefix: string,
+    commandIdOutputVariable: string
+}
 
-    // task parameters
-    public documentName: string
-    public documentParameters: string
-    public serviceRoleARN: string
-    public comment: string
-    public instanceSelector: string
-    public instanceIds: string[]
-    public instanceTags: string[]
-    public instanceBuildVariable: string
-    public maxConcurrency: string
-    public maxErrors: string
-    public timeout: string
-    public notificationArn: string
-    public notificationEvents: string
-    public notificationType: string
-    public outputS3BucketName: string
-    public outputS3KeyPrefix: string
-    public commandIdOutputVariable: string
-
-    public constructor() {
-        super()
-        try {
-            this.documentName = tl.getInput('documentName', true)
-            this.documentParameters = tl.getInput('documentParameters', false)
-            this.serviceRoleARN = tl.getInput('serviceRoleARN', false)
-            this.comment = tl.getInput('comment', false)
-
-            this.instanceSelector = tl.getInput('instanceSelector', true)
-            switch (this.instanceSelector) {
-                case TaskParameters.fromInstanceIds: {
-                    this.instanceIds = tl.getDelimitedInput('instanceIds', '\n', true)
-                }
-                                                     break
-
-                case TaskParameters.fromTags: {
-                    this.instanceTags = tl.getDelimitedInput('instanceTags', '\n', true)
-                }
-                                              break
-
-                case TaskParameters.fromBuildVariable: {
-                    this.instanceBuildVariable = tl.getInput('instanceBuildVariable', true)
-                }
-                                                       break
-
-                default:
-                    throw new Error(`Unknown value for instances selection: ${this.instanceSelector}`)
-            }
-
-            this.maxConcurrency = tl.getInput('maxConcurrency', false)
-            this.maxErrors = tl.getInput('maxErrors', false)
-            this.timeout = tl.getInput('timeout', false)
-
-            this.notificationArn = tl.getInput('notificationArn', false)
-            this.notificationEvents = tl.getInput('notificationEvents', false)
-            this.notificationType = tl.getInput('notificationType', false)
-
-            this.outputS3BucketName = tl.getInput('outputS3BucketName', false)
-            this.outputS3KeyPrefix = tl.getInput('outputS3KeyPrefix', false)
-            this.commandIdOutputVariable = tl.getInput('commandIdOutputVariable', false)
-        } catch (error) {
-            throw new Error(error.message)
-        }
+export function buildTaskParameters(): TaskParameters {
+    const parameters: TaskParameters = {
+        awsConnectionParameters: buildConnectionParameters()
     }
+
+    this.documentName = tl.getInput('documentName', true)
+    this.documentParameters = tl.getInput('documentParameters', false)
+    this.serviceRoleARN = tl.getInput('serviceRoleARN', false)
+    this.comment = tl.getInput('comment', false)
+
+    this.instanceSelector = tl.getInput('instanceSelector', true)
+    switch (this.instanceSelector) {
+        case TaskParameters.fromInstanceIds: {
+            this.instanceIds = tl.getDelimitedInput('instanceIds', '\n', true)
+        }
+                                             break
+
+        case TaskParameters.fromTags: {
+            this.instanceTags = tl.getDelimitedInput('instanceTags', '\n', true)
+        }
+                                      break
+
+        case TaskParameters.fromBuildVariable: {
+            this.instanceBuildVariable = tl.getInput('instanceBuildVariable', true)
+        }
+                                               break
+
+        default:
+            throw new Error(`Unknown value for instances selection: ${this.instanceSelector}`)
+    }
+
+    this.maxConcurrency = tl.getInput('maxConcurrency', false)
+    this.maxErrors = tl.getInput('maxErrors', false)
+    this.timeout = tl.getInput('timeout', false)
+
+    this.notificationArn = tl.getInput('notificationArn', false)
+    this.notificationEvents = tl.getInput('notificationEvents', false)
+    this.notificationType = tl.getInput('notificationType', false)
+
+    this.outputS3BucketName = tl.getInput('outputS3BucketName', false)
+    this.outputS3KeyPrefix = tl.getInput('outputS3KeyPrefix', false)
+    this.commandIdOutputVariable = tl.getInput('commandIdOutputVariable', false)
+
+    return parameters
 }
