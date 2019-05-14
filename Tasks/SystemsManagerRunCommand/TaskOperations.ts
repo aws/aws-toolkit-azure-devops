679563f2d963669c30601a606e992314ffed049f
Hunter Werlla <werlla@amazon.com>
2019-05-14 15:54:31 -0700

    more delete

--- Tasks/SystemsManagerRunCommand/TaskOperations.ts
+++ Tasks/SystemsManagerRunCommand/TaskOperations.ts
@@ -1,28 +1,21 @@
-/*
-  Copyright 2017-2018 Amazon.com, Inc. and its affiliates. All Rights Reserved.
-  *
-  * Licensed under the MIT License. See the LICENSE accompanying this file
-  * for the specific language governing permissions and limitations under
-  * the License.
-  */
+/*!
+ * Copyright 2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: MIT
+ */
 
-import tl = require('vsts-task-lib/task');
-import path = require('path');
-import SSM = require('aws-sdk/clients/ssm');
-import { AWSError } from 'aws-sdk/lib/error';
-import { SdkUtils } from 'sdkutils/sdkutils';
-import { TaskParameters } from './RunCommandTaskParameters';
+import SSM = require('aws-sdk/clients/ssm')
+import { SdkUtils } from 'Common/sdkutils'
+import tl = require('vsts-task-lib/task')
+import { TaskParameters } from './TaskParameters'
 
 export class TaskOperations {
-
     public constructor(
+        public readonly ssmClient: SSM,
         public readonly taskParameters: TaskParameters
     ) {
     }
 
     public async execute(): Promise<void> {
-        await this.createServiceClients();
-
         const request: SSM.SendCommandRequest = {
             DocumentName: this.taskParameters.documentName,
             Comment: this.taskParameters.comment,
@@ -32,37 +25,38 @@ export class TaskOperations {
             TimeoutSeconds: parseInt(this.taskParameters.timeout, 10),
             OutputS3BucketName: this.taskParameters.outputS3BucketName,
             OutputS3KeyPrefix: this.taskParameters.outputS3KeyPrefix
-        };
+        }
 
         if (this.taskParameters.documentParameters) {
-            request.Parameters = JSON.parse(this.taskParameters.documentParameters);
+            request.Parameters = JSON.parse(this.taskParameters.documentParameters)
         }
 
         switch (this.taskParameters.instanceSelector) {
             case TaskParameters.fromInstanceIds: {
-                request.InstanceIds = this.taskParameters.instanceIds;
+                request.InstanceIds = this.taskParameters.instanceIds
             }
-            break;
+                                                 break
 
             case TaskParameters.fromTags: {
-                request.Targets = [];
+                request.Targets = []
                 this.taskParameters.instanceTags.forEach((it) => {
-                    const kv = it.split('=');
-                    const t: SSM.Target = {};
-                    t.Key = 'tag:' + kv[0].trim();
-                    t.Values = kv[1].split(',');
-                    request.Targets.push(t);
-                });
+                    const kv = it.split('=')
+                    const t: SSM.Target = {}
+                    t.Key = 'tag:' + kv[0].trim()
+                    t.Values = kv[1].split(',')
+                    request.Targets.push(t)
+                })
             }
-            break;
+                                          break
 
             case TaskParameters.fromBuildVariable: {
-                const instanceIds = tl.getVariable(this.taskParameters.instanceBuildVariable);
+                const instanceIds = tl.getVariable(this.taskParameters.instanceBuildVariable)
                 if (instanceIds) {
-                    request.InstanceIds = instanceIds.trim().split(',');
+                    request.InstanceIds = instanceIds.trim().split(',')
                 } else {
-                    throw new Error(tl.loc('InstanceIdsFromVariableFailed', this.taskParameters.instanceBuildVariable));
+                    throw new Error(tl.loc('InstanceIdsFromVariableFailed', this.taskParameters.instanceBuildVariable))
                 }
+                break
             }
         }
 
@@ -71,25 +65,15 @@ export class TaskOperations {
                 NotificationArn: this.taskParameters.notificationArn,
                 NotificationEvents: [ this.taskParameters.notificationEvents ],
                 NotificationType: this.taskParameters.notificationType
-            };
+            }
         }
 
-        const response = await this.ssmClient.sendCommand(request).promise();
+        const response = await this.ssmClient.sendCommand(request).promise()
         if (this.taskParameters.commandIdOutputVariable) {
-            console.log(tl.loc('SettingOutputVariable', this.taskParameters.commandIdOutputVariable));
-            tl.setVariable(this.taskParameters.commandIdOutputVariable, response.Command.CommandId);
+            console.log(tl.loc('SettingOutputVariable', this.taskParameters.commandIdOutputVariable))
+            tl.setVariable(this.taskParameters.commandIdOutputVariable, response.Command.CommandId)
         }
 
-        console.log(tl.loc('TaskCompleted', this.taskParameters.documentName, response.Command.CommandId));
-    }
-
-    private ssmClient: SSM;
-
-    private async createServiceClients(): Promise<void> {
-
-        const ssmOpts: SSM.ClientConfiguration = {
-            apiVersion: '2014-11-06'
-        };
-        this.ssmClient = await SdkUtils.createAndConfigureSdkClient(SSM, ssmOpts, this.taskParameters, tl.debug);
+        console.log(tl.loc('TaskCompleted', this.taskParameters.documentName, response.Command.CommandId))
     }
 }
--- Tasks/SystemsManagerRunCommand/TaskParameters.ts
+++ Tasks/SystemsManagerRunCommand/TaskParameters.ts
@@ -1,82 +1,79 @@
-/*
-  Copyright 2017-2018 Amazon.com, Inc. and its affiliates. All Rights Reserved.
-  *
-  * Licensed under the MIT License. See the LICENSE accompanying this file
-  * for the specific language governing permissions and limitations under
-  * the License.
-  */
+/*!
+ * Copyright 2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: MIT
+ */
 
-  import tl = require('vsts-task-lib/task');
-  import { AWSTaskParametersBase } from 'sdkutils/awsTaskParametersBase';
+import { AWSTaskParametersBase } from 'sdkutils/awsTaskParametersBase'
+import tl = require('vsts-task-lib/task')
 
-  export class TaskParameters extends AWSTaskParametersBase {
+export class TaskParameters extends AWSTaskParametersBase {
 
     // instance selector options
-    public static readonly fromInstanceIds: string = 'fromInstanceIds';
-    public static readonly fromTags: string = 'fromTags';
-    public static readonly fromBuildVariable: string = 'fromBuildVariable';
+    public static readonly fromInstanceIds: string = 'fromInstanceIds'
+    public static readonly fromTags: string = 'fromTags'
+    public static readonly fromBuildVariable: string = 'fromBuildVariable'
 
     // task parameters
-    public documentName: string;
-    public documentParameters: string;
-    public serviceRoleARN: string;
-    public comment: string;
-    public instanceSelector: string;
-    public instanceIds: string[];
-    public instanceTags: string[];
-    public instanceBuildVariable: string;
-    public maxConcurrency: string;
-    public maxErrors: string;
-    public timeout: string;
-    public notificationArn: string;
-    public notificationEvents: string;
-    public notificationType: string;
-    public outputS3BucketName: string;
-    public outputS3KeyPrefix: string;
-    public commandIdOutputVariable: string;
+    public documentName: string
+    public documentParameters: string
+    public serviceRoleARN: string
+    public comment: string
+    public instanceSelector: string
+    public instanceIds: string[]
+    public instanceTags: string[]
+    public instanceBuildVariable: string
+    public maxConcurrency: string
+    public maxErrors: string
+    public timeout: string
+    public notificationArn: string
+    public notificationEvents: string
+    public notificationType: string
+    public outputS3BucketName: string
+    public outputS3KeyPrefix: string
+    public commandIdOutputVariable: string
 
-    constructor() {
-        super();
+    public constructor() {
+        super()
         try {
-            this.documentName = tl.getInput('documentName', true);
-            this.documentParameters = tl.getInput('documentParameters', false);
-            this.serviceRoleARN = tl.getInput('serviceRoleARN', false);
-            this.comment = tl.getInput('comment', false);
+            this.documentName = tl.getInput('documentName', true)
+            this.documentParameters = tl.getInput('documentParameters', false)
+            this.serviceRoleARN = tl.getInput('serviceRoleARN', false)
+            this.comment = tl.getInput('comment', false)
 
-            this.instanceSelector = tl.getInput('instanceSelector', true);
+            this.instanceSelector = tl.getInput('instanceSelector', true)
             switch (this.instanceSelector) {
                 case TaskParameters.fromInstanceIds: {
-                    this.instanceIds = tl.getDelimitedInput('instanceIds', '\n', true);
+                    this.instanceIds = tl.getDelimitedInput('instanceIds', '\n', true)
                 }
-                break;
+                                                     break
 
                 case TaskParameters.fromTags: {
-                    this.instanceTags = tl.getDelimitedInput('instanceTags', '\n', true);
+                    this.instanceTags = tl.getDelimitedInput('instanceTags', '\n', true)
                 }
-                break;
+                                              break
 
                 case TaskParameters.fromBuildVariable: {
-                    this.instanceBuildVariable = tl.getInput('instanceBuildVariable', true);
+                    this.instanceBuildVariable = tl.getInput('instanceBuildVariable', true)
                 }
-                break;
+                                                       break
 
                 default:
-                    throw new Error(`Unknown value for instances selection: ${this.instanceSelector}`);
+                    throw new Error(`Unknown value for instances selection: ${this.instanceSelector}`)
             }
 
-            this.maxConcurrency = tl.getInput('maxConcurrency', false);
-            this.maxErrors = tl.getInput('maxErrors', false);
-            this.timeout = tl.getInput('timeout', false);
+            this.maxConcurrency = tl.getInput('maxConcurrency', false)
+            this.maxErrors = tl.getInput('maxErrors', false)
+            this.timeout = tl.getInput('timeout', false)
 
-            this.notificationArn = tl.getInput('notificationArn', false);
-            this.notificationEvents = tl.getInput('notificationEvents', false);
-            this.notificationType = tl.getInput('notificationType', false);
+            this.notificationArn = tl.getInput('notificationArn', false)
+            this.notificationEvents = tl.getInput('notificationEvents', false)
+            this.notificationType = tl.getInput('notificationType', false)
 
-            this.outputS3BucketName = tl.getInput('outputS3BucketName', false);
-            this.outputS3KeyPrefix = tl.getInput('outputS3KeyPrefix', false);
-            this.commandIdOutputVariable = tl.getInput('commandIdOutputVariable', false);
+            this.outputS3BucketName = tl.getInput('outputS3BucketName', false)
+            this.outputS3KeyPrefix = tl.getInput('outputS3KeyPrefix', false)
+            this.commandIdOutputVariable = tl.getInput('commandIdOutputVariable', false)
         } catch (error) {
-            throw new Error(error.message);
+            throw new Error(error.message)
         }
     }
 }
