81d887f5bf98d716c339398d8c5428ece7c8f0d0
Hunter Werlla <werlla@amazon.com>
2019-05-15 14:43:42 -0700

    finished tests

--- tests/TaskTests/SystemsManagerSetParameter/SystemsManagerSetParameter-test.ts
+++ tests/TaskTests/SystemsManagerSetParameter/SystemsManagerSetParameter-test.ts
@@ -6,7 +6,7 @@
 import { SSM } from 'aws-sdk'
 import { SdkUtils } from 'Common/sdkutils'
 import { TaskOperations } from '../../../Tasks/SystemsManagerSetParameter/TaskOperations'
-import { TaskParameters } from '../../../Tasks/SystemsManagerSetParameter/TaskParameters'
+import { TaskParameters, secureStringType } from '../../../Tasks/SystemsManagerSetParameter/TaskParameters'
 
 const defaultTaskParameters: TaskParameters = {
     awsConnectionParameters: undefined,
@@ -15,6 +15,50 @@ const defaultTaskParameters: TaskParameters = {
     parameterValue: undefined,
     encryptionKeyId: undefined
 }
+
+const getParameterFails = {
+    promise: function() {
+        throw {
+            code: 'Internal failure'
+        }
+    }
+}
+
+const getParameterNotFound = {
+    promise: function() {
+        throw {
+            code: 'ParameterNotFound'
+        }
+    }
+}
+
+const getParameterNotSecureString = {
+    promise: function() {
+        return {
+            Parameter: {
+                Type: 'not very secure'
+            }
+        }
+    }
+}
+
+const getParameterSecureString = {
+    promise: function() {
+        return {
+            Parameter: {
+                Type: secureStringType
+            }
+        }
+    }
+}
+
+const putParameterSucceeds = {
+    promise: function() { }
+}
+
+// unsafe any's is how jest mocking works, so this needs to be disabled for all test files
+// tslint:disable: no-unsafe-any
+jest.mock('aws-sdk')
 
 describe('Secrets Manger Get Secret', () => {
     // TODO https://github.com/aws/aws-vsts-tools/issues/167
@@ -26,7 +70,57 @@ describe('Secrets Manger Get Secret', () => {
         expect(new TaskOperations(new SSM(), defaultTaskParameters)).not.toBeNull()
     })
 
-    test(('it works or something'), () => {
-        return undefined
+    test(('fails checking parameter, fails task'), async () => {
+        expect.assertions(2)
+        const ssm = new SSM() as any
+        ssm.getParameter = jest.fn(() => getParameterFails)
+        const taskOperation = new TaskOperations(ssm, defaultTaskParameters)
+        await taskOperation
+            .execute()
+            .catch((e) => expect(`${e}`).toContain('An error occurred while accessing the parameter'))
+        expect(ssm.getParameter).toBeCalledTimes(1)
+    })
+
+    test(('fails, fails task'), async () => {
+        expect.assertions(3)
+        const ssm = new SSM() as any
+        ssm.getParameter = jest.fn(() => getParameterNotFound)
+        ssm.putParameter = jest.fn(() => getParameterNotFound)
+        const taskOperation = new TaskOperations(ssm, defaultTaskParameters)
+        await taskOperation
+            .execute()
+            .catch((e) => expect(`${e}`).toContain('Create or update of parameter failed with error'))
+        expect(ssm.getParameter).toBeCalledTimes(1)
+        expect(ssm.putParameter).toBeCalledTimes(1)
+    })
+
+    test(('checks if it exists yet, is secure string'), async () => {
+        expect.assertions(3)
+        const ssm = new SSM() as any
+        ssm.getParameter = jest.fn(() => getParameterSecureString)
+        ssm.putParameter = jest.fn((argument) => {
+           expect(argument.Type).toBe('SecureString')
+
+           return putParameterSucceeds
+        })
+        const taskOperation = new TaskOperations(ssm, defaultTaskParameters)
+        await taskOperation.execute()
+        expect(ssm.getParameter).toBeCalledTimes(1)
+        expect(ssm.putParameter).toBeCalledTimes(1)
+    })
+
+    test(('checks if it exists yet, is not secure string'), async () => {
+        expect.assertions(3)
+        const ssm = new SSM() as any
+        ssm.getParameter = jest.fn(() => getParameterNotSecureString)
+        ssm.putParameter = jest.fn((argument) => {
+            expect(argument.Type).toBe(undefined)
+
+            return putParameterSucceeds
+         })
+        const taskOperation = new TaskOperations(ssm, defaultTaskParameters)
+        await taskOperation.execute()
+        expect(ssm.getParameter).toBeCalledTimes(1)
+        expect(ssm.putParameter).toBeCalledTimes(1)
     })
 })
