97d17ae540d100258816d7115dbb705d04b756c7
Hunter Werlla <werlla@amazon.com>
2019-05-16 15:36:50 -0700

    working on refactor

---
+++ Tasks/CloudFormationCreateOrUpdateStack/TaskParameters.ts
@@ -0,0 +1,223 @@
+/*!
+ * Copyright 2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: MIT
+ */
+
+import fs = require('fs')
+import { AWSTaskParametersBase } from 'sdkutils/awsTaskParametersBase'
+import tl = require('vsts-task-lib/task')
+
+export class TaskParameters extends AWSTaskParametersBase {
+
+    public static readonly fileSource: string = 'file'
+    public static readonly urlSource: string = 'url'
+    public static readonly s3Source: string = 's3'
+    public static readonly usePreviousTemplate: string = 'usePrevious'
+
+    public static readonly maxRollbackTriggers: number = 5
+    public static readonly maxTriggerMonitoringTime: number = 180
+
+    public static readonly loadTemplateParametersFromFile: string = 'file'
+    public static readonly loadTemplateParametersInline: string = 'inline'
+
+    public static readonly ignoreStackOutputs: string = 'ignore'
+    public static readonly stackOutputsAsVariables: string = 'asVariables'
+    public static readonly stackOutputsAsJson: string = 'asJSON'
+
+    public static readonly defaultTimeoutInMins: number = 60
+
+    public stackName: string
+    public templateSource: string
+    public templateFile: string
+    public s3BucketName: string
+    public s3ObjectKey: string
+    public templateUrl: string
+    public templateParametersSource: string
+    public templateParametersFile: string
+    public templateParameters: string
+    public useChangeSet: boolean
+    public changeSetName: string
+    public description: string
+    public autoExecuteChangeSet: boolean
+    public capabilityIAM: boolean
+    public capabilityNamedIAM: boolean
+    public capabilityAutoExpand: boolean
+    public roleARN: string
+    public notificationARNs: string[]
+    public resourceTypes: string[]
+    public tags: string[]
+    public monitorRollbackTriggers: boolean
+    public monitoringTimeInMinutes: number = 0
+    public rollbackTriggerARNs: string[]
+
+    public onFailure: string
+    public warnWhenNoWorkNeeded: boolean
+    public outputVariable: string
+    public captureStackOutputs: string
+    public captureAsSecuredVars: boolean
+
+    public timeoutInMins: number = TaskParameters.defaultTimeoutInMins
+
+    public constructor() {
+        super()
+        try {
+            this.stackName = tl.getInput('stackName', true)
+
+            this.templateSource = tl.getInput('templateSource', true)
+            switch (this.templateSource) {
+                case TaskParameters.fileSource: {
+                    this.templateFile = tl.getPathInput('templateFile', true, true)
+                    this.s3BucketName = tl.getInput('s3BucketName', false)
+                }
+                                                break
+
+                case TaskParameters.urlSource: {
+                    this.templateUrl = tl.getInput('templateUrl', true)
+                }
+                                               break
+
+                case TaskParameters.s3Source: {
+                    this.s3BucketName = tl.getInput('s3BucketName', true)
+                    this.s3ObjectKey = tl.getInput('s3ObjectKey', true)
+                }
+                                              break
+
+                default:
+                    throw new Error(`Unrecognized template source: ${this.templateSource}`)
+            }
+
+            this.templateParametersSource = tl.getInput('templateParametersSource', true)
+            switch (this.templateParametersSource) {
+                case TaskParameters.loadTemplateParametersFromFile: {
+                    // Value set optional for backwards compatibilty, to enable continued operation of
+                    // tasks configured before 'inline' mode was added.
+                    // Note that if the user does not give a value then instead of an empty/null
+                    // path (per default value for the field), we get what appears to be the root
+                    // of the repository path. To solve this without needing to add a task parameter
+                    // to indicate we should use a parameter file (a breaking change) we do a simple
+                    // directory vs file test
+                    this.templateParametersFile = tl.getPathInput('templateParametersFile', false, true)
+                    if (this.templateParametersFile) {
+                        if (fs.statSync(this.templateParametersFile).isDirectory()) {
+                            this.templateParametersFile = null
+                        }
+                    }
+                    break
+                }
+
+                case TaskParameters.loadTemplateParametersInline: {
+                    this.templateParameters = tl.getInput('templateParameters', true)
+                    break
+                }
+
+                default:
+                    throw new Error(`Unrecognized template parameters source: ${this.templateParametersSource}`)
+            }
+
+            this.useChangeSet = tl.getBoolInput('useChangeSet', false)
+            this.changeSetName = tl.getInput('changeSetName', this.useChangeSet)
+            this.description = tl.getInput('description', false)
+            this.autoExecuteChangeSet = tl.getBoolInput('autoExecuteChangeSet', false)
+
+            this.capabilityIAM = tl.getBoolInput('capabilityIAM', false)
+            this.capabilityNamedIAM = tl.getBoolInput('capabilityNamedIAM', false)
+            this.capabilityAutoExpand = tl.getBoolInput('capabilityAutoExpand', false)
+
+            this.roleARN = tl.getInput('roleARN', false)
+            this.tags = tl.getDelimitedInput('tags', '\n', false)
+            this.notificationARNs = tl.getDelimitedInput('notificationARNs', '\n', false)
+            this.resourceTypes = tl.getDelimitedInput('resourceTypes', '\n', false)
+
+            this.monitorRollbackTriggers = tl.getBoolInput('monitorRollbackTriggers', false)
+            if (this.monitorRollbackTriggers) {
+                const t = tl.getInput('monitoringTimeInMinutes', false)
+                if (t) {
+                    this.monitoringTimeInMinutes = parseInt(t, 10)
+                    if (this.monitoringTimeInMinutes < 0 || this.monitoringTimeInMinutes > TaskParameters.maxTriggerMonitoringTime) {
+                        throw new Error(tl.loc('InvalidTriggerMonitoringTime', this.monitoringTimeInMinutes, TaskParameters.maxTriggerMonitoringTime))
+                    }
+                }
+                this.rollbackTriggerARNs = tl.getDelimitedInput('rollbackTriggerARNs', '\n', false)
+                if (this.rollbackTriggerARNs && this.rollbackTriggerARNs.length > TaskParameters.maxRollbackTriggers) {
+                    throw new Error(tl.loc('ExceededMaxRollbackTriggers', this.rollbackTriggerARNs.length, TaskParameters.maxRollbackTriggers))
+                }
+            }
+
+            this.onFailure = tl.getInput('onFailure')
+            this.warnWhenNoWorkNeeded = tl.getBoolInput('warnWhenNoWorkNeeded')
+            this.outputVariable = tl.getInput('outputVariable', false)
+            this.captureStackOutputs = tl.getInput('captureStackOutputs', false)
+            this.captureAsSecuredVars = tl.getBoolInput('captureAsSecuredVars', false)
+
+            const t = tl.getInput('timeoutInMins', false)
+            if (t) {
+                const tval = parseInt(t, 10)
+                // allow for shorter periods if user wants, but filter out -ve/0 silliness
+                if (tval > 0) {
+                    this.timeoutInMins = tval
+                }
+            }
+
+        } catch (error) {
+            throw new Error(error.message)
+        }
+    }
+}
+
+export const fileSource: string = 'file'
+export const urlSource: string = 'url'
+export const s3Source: string = 's3'
+export const usePreviousTemplate: string = 'usePrevious'
+
+export const maxRollbackTriggers: number = 5
+export const maxTriggerMonitoringTime: number = 180
+
+export const loadTemplateParametersFromFile: string = 'file'
+export const loadTemplateParametersInline: string = 'inline'
+
+export const ignoreStackOutputs: string = 'ignore'
+export const stackOutputsAsVariables: string = 'asVariables'
+export const stackOutputsAsJson: string = 'asJSON'
+
+export const defaultTimeoutInMins: number = 60
+
+export interface TaskParameters {
+    awsConnectionParameters: AWSConnectionParameters,
+    stackName: string
+    templateSource: string
+    templateFile: string
+    s3BucketName: string
+    s3ObjectKey: string
+    templateUrl: string
+    templateParametersSource: string
+    templateParametersFile: string
+    templateParameters: string
+    useChangeSet: boolean
+    changeSetName: string
+    description: string
+    autoExecuteChangeSet: boolean
+    capabilityIAM: boolean
+    capabilityNamedIAM: boolean
+    capabilityAutoExpand: boolean
+    roleARN: string
+    notificationARNs: string[]
+    resourceTypes: string[]
+    tags: string[]
+    monitorRollbackTriggers: boolean
+    monitoringTimeInMinutes: number = 0
+    rollbackTriggerARNs: string[]
+    onFailure: string
+    warnWhenNoWorkNeeded: boolean
+    outputVariable: string
+    captureStackOutputs: string
+    captureAsSecuredVars: boolean
+    timeoutInMins: number = TaskParameters.defaultTimeoutInMins
+}
+
+export function buildTaskParameters(): TaskParameters {
+    const parameters: TaskParameters {
+
+    }
+
+    return parameters
+}
---
+++ Tasks/Common/cloudformationutils.ts
@@ -0,0 +1,6 @@
+/*!
+ * Copyright 2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: MIT
+ */
+
+export { CloudFormationUtils } from 'sdkutils/cloudformationutils'
