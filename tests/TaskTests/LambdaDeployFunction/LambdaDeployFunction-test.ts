d660d3a9845ddccb170bf5e851f05c67f53eec62
Hunter Werlla <werlla@amazon.com>
2019-05-16 10:05:47 -0700

    test skeleton

---
+++ tests/TaskTests/LambdaDeployFunction/LambdaDeployFunction-test.ts
@@ -0,0 +1,51 @@
+/*!
+ * Copyright 2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: MIT
+ */
+
+import { IAM, Lambda } from 'aws-sdk'
+import { SdkUtils } from '../../../Tasks/Common/sdkutils/sdkutils'
+import { TaskOperations } from '../../../Tasks/LambdaDeployFunction/TaskOperations'
+import { TaskParameters } from '../../../Tasks/LambdaDeployFunction/TaskParameters'
+
+// unsafe any's is how jest mocking works, so this needs to be disabled for all test files
+// tslint:disable: no-unsafe-any
+jest.mock('aws-sdk')
+
+const baseTaskParameters: TaskParameters = {
+    awsConnectionParameters: undefined,
+    deploymentMode: undefined,
+    functionName: undefined,
+    functionHandler: undefined,
+    runtime: undefined,
+    codeLocation: undefined,
+    localZipFile: undefined,
+    s3Bucket: undefined,
+    s3ObjectKey: undefined,
+    s3ObjectVersion: undefined,
+    roleARN: undefined,
+    description: undefined,
+    memorySize: 128,
+    timeout: 3,
+    publish: undefined,
+    deadLetterARN: undefined,
+    kmsKeyARN: undefined,
+    environment: undefined,
+    tags: undefined,
+    securityGroups: undefined,
+    subnets: undefined,
+    tracingConfig: undefined,
+    outputVariable: undefined
+}
+
+describe('Lambda Deploy Function', () => {
+    // TODO https://github.com/aws/aws-vsts-tools/issues/167
+    beforeAll(() => {
+        SdkUtils.readResourcesFromRelativePath('../../../_build/Tasks/S3Download/task.json')
+    })
+
+    test('Creates a TaskOperation', () => {
+        const taskParameters = baseTaskParameters
+        expect(new TaskOperations(new IAM(), new Lambda(), taskParameters)).not.toBeNull()
+    })
+})
