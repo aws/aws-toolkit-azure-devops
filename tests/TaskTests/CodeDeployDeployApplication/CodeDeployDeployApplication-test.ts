5a5cef158189bcebd5e9ea280220b78bb2483b57
Hunter Werlla <werlla@amazon.com>
2019-05-17 15:20:25 -0700

    add skeleton for tests

---
+++ tests/TaskTests/CodeDeployDeployApplication/CodeDeployDeployApplication-test.ts
@@ -0,0 +1,65 @@
+/*!
+ * Copyright 2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: MIT
+ */
+
+import { CodeDeploy, S3 } from 'aws-sdk'
+import { TaskOperations } from '../../../Tasks/CodeDeployDeployApplication/TaskOperations'
+import { TaskParameters } from '../../../Tasks/CodeDeployDeployApplication/TaskParameters'
+import { SdkUtils } from '../../../Tasks/Common/sdkutils/sdkutils'
+
+// unsafe any's is how jest mocking works, so this needs to be disabled for all test files
+// tslint:disable: no-unsafe-any
+jest.mock('aws-sdk')
+
+const baseTaskParameters: TaskParameters = {
+    awsConnectionParameters: undefined,
+    applicationName: undefined,
+    deploymentGroupName: undefined,
+    deploymentRevisionSource: undefined,
+    revisionBundle: undefined,
+    bucketName: undefined,
+    bundlePrefix: undefined,
+    bundleKey: undefined,
+    description: undefined,
+    fileExistsBehavior: undefined,
+    updateOutdatedInstancesOnly: undefined,
+    ignoreApplicationStopFailures: undefined,
+    outputVariable: undefined,
+    timeoutInMins: undefined
+}
+
+describe('CodeDeploy Deploy Application', () => {
+    // TODO https://github.com/aws/aws-vsts-tools/issues/167
+    beforeAll(() => {
+        SdkUtils.readResourcesFromRelativePath('../../../_build/Tasks/CodeDeployDeployApplication/task.json')
+    })
+
+    test('Creates a TaskOperation', () => {
+        expect(new TaskOperations(new CodeDeploy(), new S3(), baseTaskParameters)).not.toBeNull()
+    })
+
+    test('Verify create resources fails, fails task', () => {
+        return undefined
+    })
+
+    test('Upload needed, fails, fails task', () => {
+        return undefined
+    })
+
+    test('Upload needed, succeeds', () => {
+        return undefined
+    })
+
+    test('Upload not needed, succeeds', () => {
+        return undefined
+    })
+
+    test('Wait for deployment, fails, fails task', () => {
+        return undefined
+    })
+
+    test('Happy path', () => {
+        return undefined
+    })
+})
