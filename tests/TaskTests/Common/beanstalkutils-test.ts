9525e91affbded8b5bd909b7020183c2d44b6a91
Hunter Werlla <werlla@amazon.com>
2019-05-20 14:04:16 -0700

    working on bundle tests

--- tests/TaskTests/Common/beanstalkutils-test.ts
+++ tests/TaskTests/Common/beanstalkutils-test.ts
@@ -3,19 +3,44 @@
  * SPDX-License-Identifier: MIT
  */
 
+import { ElasticBeanstalk } from 'aws-sdk'
 import { BeanstalkUtils } from 'Common/beanstalkutils'
+import path = require('path')
+
+// unsafe any's is how jest mocking works, so this needs to be disabled for all test files
+// tslint:disable: no-unsafe-any
+jest.mock('aws-sdk')
+
+const s3BucketResponse = {
+    promise: () => ({ S3Bucket: 'bucket' })
+}
 
 describe('BeanstalkUtils', () => {
-    test('DetermineS3Bucket succeds', () => {
-        return undefined
+    test('DetermineS3Bucket succeds', async () => {
+        expect.assertions(1)
+        const beanstalk = new ElasticBeanstalk() as any
+        beanstalk.createStorageLocation = jest.fn(() => s3BucketResponse)
+        const response = await BeanstalkUtils.determineS3Bucket(beanstalk)
+        expect(response).toBe('bucket')
     })
 
-    test('DetermineS3Bucket fails, throws', () => {
-        return undefined
+    test('DetermineS3Bucket fails, throws', async () => {
+        expect.assertions(1)
+        const beanstalk = new ElasticBeanstalk() as any
+        beanstalk.createStorageLocation = jest.fn(() => {
+            throw new Error('failure')
+        })
+        await BeanstalkUtils.determineS3Bucket(beanstalk).catch(err => {
+            expect(`${err}`).toContain('Error: failure')
+        })
     })
 
-    test('PrepareAspNet bundle fails, throws', () => {
-        return undefined
+    test('PrepareAspNet bundle fails, throws', async () => {
+        const tmp = path.join(__dirname, '../../Resources/BeanstalkBundle')
+        const code = path.join(__dirname, '../../Resources/BeanstalkBundle')
+        await BeanstalkUtils.prepareAspNetCoreBundle(tmp, code).catch(err => {
+            expect(`${err}`).toContain('Error: failure')
+        })
     })
 
     test('PrepareAspNet bundle succeeds, verify output', () => {
@@ -23,7 +48,8 @@ describe('BeanstalkUtils', () => {
     })
 
     test('ConstructVersionLabel succeeds', () => {
-        return undefined
+        expect(BeanstalkUtils.constructVersionLabel('label')).toBe('label')
+        expect(BeanstalkUtils.constructVersionLabel(undefined)).toMatch(new RegExp('^v[0-9]+$'))
     })
 
     test('UploadBundle throws on failure', () => {
