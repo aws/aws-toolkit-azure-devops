ca351914c6b3f527208bc180a37065c965d01542
Hunter Werlla <werlla@amazon.com>
2019-05-21 08:45:52 -0700

    working on it

--- tests/TaskTests/BeanstalkCreateApplicationVersion/BeanstalkCreateApplicationVersion-test.ts
+++ tests/TaskTests/BeanstalkCreateApplicationVersion/BeanstalkCreateApplicationVersion-test.ts
@@ -7,16 +7,23 @@ import { ElasticBeanstalk, S3 } from 'aws-sdk'
 
 import { SdkUtils } from 'Common/sdkutils/sdkutils'
 import { TaskOperations } from '../../../Tasks/BeanstalkCreateApplicationVersion/TaskOperations'
-import { TaskParameters } from '../../../Tasks/BeanstalkCreateApplicationVersion/TaskParameters'
+import {
+    TaskParameters,
+    applicationTypeS3Archive
+} from '../../../Tasks/BeanstalkCreateApplicationVersion/TaskParameters'
 
 // unsafe any's is how jest mocking works, so this needs to be disabled for all test files
 // tslint:disable: no-unsafe-any
 jest.mock('aws-sdk')
 
+const verifyApplicationExistsResponse = {
+    promise: () => ({ Applications: ['yes'] })
+}
+
 const defaultTaskParameters: TaskParameters = {
     awsConnectionParameters: undefined,
     applicationName: undefined,
-    applicationType: undefined,
+    applicationType: applicationTypeS3Archive,
     versionLabel: undefined,
     webDeploymentArchive: undefined,
     dotnetPublishPath: undefined,
@@ -28,7 +35,7 @@ const defaultTaskParameters: TaskParameters = {
 
 // NOTE: most of the actual functionality for elastic beanstalk is in the ElasticBeanstalkUtils, so
 // most of the tests are there,
-describe('Beanstalk Deploy Application', () => {
+describe('Beanstalk Create Application Version', () => {
     // TODO https://github.com/aws/aws-vsts-tools/issues/167
     beforeAll(() => {
         SdkUtils.readResourcesFromRelativePath('../../../_build/Tasks/BeanstalkCreateApplicationVersion/task.json')
@@ -36,5 +43,36 @@ describe('Beanstalk Deploy Application', () => {
 
     test('Creates a TaskOperation', () => {
         expect(new TaskOperations(new ElasticBeanstalk(), new S3(), defaultTaskParameters)).not.toBeNull()
+    })
+
+    test('Create application version throws, fails task', async () => {
+        expect.assertions(1)
+        const beanstalk = new ElasticBeanstalk() as any
+        beanstalk.describeApplications = jest.fn(() => verifyApplicationExistsResponse)
+        beanstalk.createApplicationVersion = jest.fn(() => {
+            throw new Error('OW!')
+        })
+        const taskOperations = new TaskOperations(beanstalk, new S3(), defaultTaskParameters)
+        await taskOperations.execute().catch(err => {
+            expect(err).toStrictEqual(Error('OW!'))
+        })
+    })
+
+    test('Happy path', async () => {
+        const beanstalk = new ElasticBeanstalk() as any
+        beanstalk.describeApplications = jest.fn(() => verifyApplicationExistsResponse)
+        beanstalk.createApplicationVersion = jest.fn(() => verifyApplicationExistsResponse)
+        const taskOperations = new TaskOperations(beanstalk, new S3(), defaultTaskParameters)
+        await taskOperations.execute()
+    })
+
+    test('Happy path uploads new object to s3', async () => {
+        const beanstalk = new ElasticBeanstalk() as any
+        const taskParameters = { ...defaultTaskParameters }
+        taskParameters.applicationType = applicationTypeS3Archive
+        beanstalk.describeApplications = jest.fn(() => verifyApplicationExistsResponse)
+        beanstalk.createApplicationVersion = jest.fn(() => verifyApplicationExistsResponse)
+        const taskOperations = new TaskOperations(beanstalk, new S3(), taskParameters)
+        await taskOperations.execute()
     })
 })
