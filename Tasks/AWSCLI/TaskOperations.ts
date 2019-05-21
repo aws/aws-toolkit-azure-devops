1e270f9785a37bb4dae3f4d59a33eb886eeb59c8
Hunter Werlla <werlla@amazon.com>
2019-05-21 10:55:14 -0700

    refactor awscli

---
+++ Tasks/AWSCLI/TaskOperations.ts
@@ -0,0 +1,77 @@
+/*!
+ * Copyright 2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: MIT
+ */
+
+import { getCredentials, getRegion } from 'Common/awsConnectionParameters'
+import { SdkUtils } from 'Common/sdkutils'
+import tl = require('vsts-task-lib/task')
+import tr = require('vsts-task-lib/toolrunner')
+import Parameters = require('./TaskParameters')
+
+export class TaskOperations {
+
+    public constructor(
+        public readonly taskParameters: Parameters.TaskParameters
+    ) {}
+
+    public async execute(): Promise<void> {
+        try {
+            await this.configureAwsCli()
+            await SdkUtils.configureHttpProxyFromAgentProxyConfiguration('AWSCLI')
+
+            const awsCliPath = tl.which('aws')
+            const awsCliTool: tr.ToolRunner = tl.tool(awsCliPath)
+            awsCliTool.arg(this.taskParameters.awsCliCommand)
+            awsCliTool.arg(this.taskParameters.awsCliSubCommand)
+            if (this.taskParameters.awsCliParameters !== undefined) {
+                awsCliTool.line(this.taskParameters.awsCliParameters)
+            }
+            const code: number = await awsCliTool
+                .exec({ failOnStdErr: this.taskParameters.failOnStandardError } as any)
+            tl.debug(`return code: ${code}`)
+            if (code !== 0) {
+                tl.setResult(tl.TaskResult.Failed, tl.loc('AwsReturnCode', awsCliTool, code))
+            } else {
+                tl.setResult(tl.TaskResult.Succeeded, tl.loc('AwsReturnCode', awsCliTool, code))
+            }
+        } catch (err) {
+            tl.setResult(tl.TaskResult.Failed, (err as Error).message)
+        }
+    }
+
+    // If assume role credentials are in play, make sure the initial generation
+    // of temporary credentials has been performed. If no credentials and/or
+    // region were defined then we assume they are already set in the host
+    // environment. Environment variables are preferred over stored profiles
+    // as this isolates parallel builds and avoids content left lying around on
+    // the agent when a build completes
+    private async configureAwsCli() {
+        const env = process.env
+
+        const credentials = await getCredentials(this.taskParameters.awsConnectionParameters)
+        if (credentials) {
+            await credentials.getPromise()
+            tl.debug('configure credentials into environment variables')
+            env.AWS_ACCESS_KEY_ID = credentials.accessKeyId
+            env.AWS_SECRET_ACCESS_KEY = credentials.secretAccessKey
+            if (credentials.sessionToken) {
+                env.AWS_SESSION_TOKEN = credentials.sessionToken
+            }
+        }
+
+        const region = await getRegion()
+        if (region) {
+            tl.debug('configure region into environment variable')
+            env.AWS_DEFAULT_REGION = region
+        }
+    }
+
+    public static checkIfAwsCliIsInstalled() {
+        try {
+            return !!tl.which('aws', true)
+        } catch (error) {
+            tl.setResult(tl.TaskResult.Failed, tl.loc('AWSCLINotInstalled'))
+        }
+    }
+}
