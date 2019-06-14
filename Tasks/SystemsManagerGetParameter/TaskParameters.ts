9fc56a19f5a118fde813b8635c27a1df65ef453c
Hunter Werlla <werlla@amazon.com>
2019-06-14 16:01:07 -0700

    SystemsManagerGetParameter fixed

--- Tasks/SystemsManagerGetParameter/TaskParameters.ts
+++ Tasks/SystemsManagerGetParameter/TaskParameters.ts
@@ -4,16 +4,16 @@
  */
 
 import { AWSConnectionParameters, buildConnectionParameters } from 'Common/awsConnectionParameters'
-import { readModeSingle } from 'Common/ssm'
+import { readModeHierarchy, readModeSingle } from 'Common/ssm'
 import tl = require('vsts-task-lib/task')
 
 export interface TaskParameters {
     awsConnectionParameters: AWSConnectionParameters
     readMode: string
     parameterName: string
-    parameterVersion: number
+    parameterVersion: number | undefined
     parameterPath: string
-    recursive: boolean
+    recursive: boolean | undefined
     variableNameTransform: string
     customVariableName: string
     replacementPattern: string
@@ -26,41 +26,46 @@ export function buildTaskParameters(): TaskParameters {
     const parameters: TaskParameters = {
         awsConnectionParameters: buildConnectionParameters(),
         readMode: tl.getInput('readMode', true),
-        parameterName: undefined,
+        parameterName: '',
         parameterVersion: undefined,
-        parameterPath: undefined,
+        parameterPath: '',
         recursive: undefined,
-        variableNameTransform: undefined,
-        customVariableName: undefined,
-        replacementPattern: undefined,
-        replacementText: undefined,
-        globalMatch: undefined,
-        caseInsensitiveMatch: undefined
+        variableNameTransform: '',
+        customVariableName: '',
+        replacementPattern: '',
+        replacementText: '',
+        globalMatch: false,
+        caseInsensitiveMatch: false
     }
 
-    if (parameters.readMode === readModeSingle) {
-        parameters.parameterName = tl.getInput('parameterName', true)
-        const versionstring = tl.getInput('parameterVersion', false)
-        if (versionstring) {
-            const pv = parseInt(versionstring, 10)
-            if (pv > 0) {
-                parameters.parameterVersion = pv
-            } else {
-                throw new Error(tl.loc('InvalidParameterVersion', pv))
+    switch (parameters.readMode) {
+        case readModeSingle:
+            parameters.parameterName = tl.getInput('parameterName', true)
+            const versionstring = tl.getInput('parameterVersion', false)
+            if (versionstring) {
+                const pv = parseInt(versionstring, 10)
+                if (pv > 0) {
+                    parameters.parameterVersion = pv
+                } else {
+                    throw new Error(tl.loc('InvalidParameterVersion', pv))
+                }
             }
-        }
-        parameters.variableNameTransform = tl.getInput('singleNameTransform', false)
-    } else {
-        parameters.parameterPath = tl.getInput('parameterPath', true)
-        parameters.recursive = tl.getBoolInput('recursive', false)
-        parameters.variableNameTransform = tl.getInput('hierarchyNameTransform', false)
+            parameters.variableNameTransform = tl.getInput('singleNameTransform', false)
+            break
+        case readModeHierarchy:
+            parameters.parameterPath = tl.getInput('parameterPath', true)
+            parameters.recursive = tl.getBoolInput('recursive', false)
+            parameters.variableNameTransform = tl.getInput('hierarchyNameTransform', false)
+            break
+        default:
+            throw new Error(tl.loc('UnknownReadMode', parameters.readMode))
     }
 
     switch (parameters.variableNameTransform) {
         case 'substitute':
             parameters.replacementPattern = tl.getInput('replacementPattern', true)
             parameters.replacementText = tl.getInput('replacementText', false) || ''
-            parameters.globalMatch = tl.getBoolInput('globalMatch', false)
+            parameters.globalMatch = tl.getBoolInput('globalMatch', false) || false
             parameters.caseInsensitiveMatch = tl.getBoolInput('caseInsensitiveMatch', false)
             break
 
