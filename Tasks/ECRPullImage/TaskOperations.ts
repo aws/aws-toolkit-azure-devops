d543992df334685231935fa694ec743ec2a1ef6c
Hunter Werlla <werlla@amazon.com>
2019-07-05 14:18:58 -0700

    start to work on pull task

--- Tasks/ECRPullImage/TaskOperations.ts
+++ Tasks/ECRPullImage/TaskOperations.ts
@@ -3,6 +3,14 @@
  * SPDX-License-Identifier: MIT
  */
 
+/*
+  Copyright 2017-2018 Amazon.com, Inc. and its affiliates. All Rights Reserved.
+  *
+  * Licensed under the MIT License. See the LICENSE accompanying this file
+  * for the specific language governing permissions and limitations under
+  * the License.
+  */
+
 import ECR = require('aws-sdk/clients/ecr')
 import base64 = require('base-64')
 import { DockerHandler } from 'Common/dockerUtils'
---
+++ Tasks/ECRPullImage/TaskParameters.ts
@@ -0,0 +1,48 @@
+/*!
+ * Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: MIT
+ */
+
+import { AWSConnectionParameters, buildConnectionParameters } from 'Common/awsConnectionParameters'
+import tl = require('vsts-task-lib/task')
+
+export const imageNameSource: string = 'imagename'
+export const imageIdSource: string = 'imageid'
+
+export interface TaskParameters {
+    awsConnectionParameters: AWSConnectionParameters
+    imageSource: string
+    sourceImageName: string
+    sourceImageTag: string
+    sourceImageId: string
+    repositoryName: string
+    pushTag: string
+    autoCreateRepository: boolean
+    outputVariable: string
+}
+
+export function buildTaskParameters(): TaskParameters {
+    const parameters: TaskParameters = {
+        awsConnectionParameters: buildConnectionParameters(),
+        imageSource: tl.getInput('imageSource', true),
+        repositoryName: tl.getInput('repositoryName', true),
+        pushTag: tl.getInput('pullTag', false),
+        autoCreateRepository: tl.getBoolInput('autoCreateRepository', false),
+        outputVariable: tl.getInput('outputVariable', false),
+        sourceImageName: undefined,
+        sourceImageId: undefined,
+        sourceImageTag: undefined
+    }
+
+    if (parameters.imageSource === imageNameSource) {
+        parameters.sourceImageName = tl.getInput('sourceImageName', true)
+        parameters.sourceImageTag = tl.getInput('sourceImageTag', false)
+        if (!parameters.sourceImageTag) {
+            parameters.sourceImageTag = 'latest'
+        }
+    } else {
+        parameters.sourceImageId = tl.getInput('sourceImageId', true)
+    }
+
+    return parameters
+}
