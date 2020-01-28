aws/aws-toolkit-azure-devops

commit 3b31e68faab5aac3836068d5b52ca30e97e449ed

Delete second version (#310)

build-scripts/generateResources.js
@@ -13,7 +13,6 @@ const folders = require('./scriptUtils')
 const timeMessage = 'Generated resources'
 const taskJson = 'task.json'
 const taskLocJson = 'task.loc.json'
-const masterVersionFile = '_versioninfo.json'
 const vssPath = path.join(folders.repoRoot, 'vss-extension.json')
 const vssBuildPath = path.join(folders.repoRoot, '_build', 'vss-extension.json')
 
@@ -265,16 +264,14 @@ function generateTaskResources(taskPath, knownRegions, versionInfo) {
 
 function addVersionToVssExtension(versionInfo) {
     var vss = JSON.parse(fs.readFileSync(vssPath))
-    vss.version = '' + versionInfo.Major + '.' + versionInfo.Minor + '.' + versionInfo.Patch
+    vss.version = versionInfo
     fs.writeFileSync(vssBuildPath, JSON.stringify(vss, null, 2))
 }
 
 console.time(timeMessage)
-var versionInfoFile = path.join(folders.repoRoot, masterVersionFile)
-var versionInfo = JSON.parse(fs.readFileSync(versionInfoFile))
 const knownRegions = fetchLatestRegions()
 findMatchingFiles(folders.sourceTasks).forEach(path => {
-    generateTaskResources(path, knownRegions, versionInfo)
+    generateTaskResources(path, knownRegions, folders.releaseVersion)
 })
-addVersionToVssExtension(versionInfo)
+addVersionToVssExtension(folders.releaseVersion)
 console.timeEnd(timeMessage)

build-scripts/copyResources.ts
@@ -3,25 +3,27 @@
  * SPDX-License-Identifier: MIT
  */
 
-const folders = require('./scriptUtils')
-const fs = require('fs-extra')
+import fs = require('fs-extra')
+import folders = require('./scriptUtils')
 
 const timeMessage = 'Copied resources'
 const ignoredFiles = ['tsconfig.json', 'make.json']
 const ignoreExtensions = ['.ts']
 
-const filterFunc = (src, dest) => {
+const filterFunc = (src: string, dest: string) => {
     return (
         ignoredFiles.every(element => {
             if (src.endsWith(element)) {
                 return false
             }
+
             return true
         }) &&
         ignoreExtensions.every(element => {
             if (src.endsWith(element)) {
                 return false
             }
+
             return true
         })
     )
@@ -33,8 +35,8 @@ const options = {
 }
 
 console.time(timeMessage)
-console.log('Copying files from ' + folders.sourceTasks + ' to ' + folders.buildTasks)
+console.log(`Copying files from ${folders.sourceTasks} to folders.buildTasks`)
 fs.copy(folders.sourceTasks, folders.buildTasks, options)
     .then(() => console.info('Successfully coppied files'))
-    .catch(error => console.info('Copy failed with error: ' + error))
+    .catch(error => console.info(`Copy failed with error: ${error}`))
 console.timeEnd(timeMessage)

build-scripts/createRelease.ts
@@ -6,11 +6,10 @@
 import * as child_process from 'child_process'
 import * as fs from 'fs-extra'
 import * as path from 'path'
+import { releaseVersion } from './scriptUtils'
 
 const changesDirectory = '.changes'
 const nextReleaseDirectory = path.join(changesDirectory, 'next-release')
-// tslint:disable-next-line:no-var-requires no-unsafe-any
-const releaseVersion = require(path.join('..', 'package.json')).version
 const changesFile = path.join(changesDirectory, `${releaseVersion}.json`)
 
 fs.mkdirpSync(nextReleaseDirectory)

build-scripts/scriptUtils.d.ts
@@ -10,6 +10,7 @@ declare module scriptUtils {
     const buildTasks: string
     const packageRoot: string
     const packageTask: string
+    const releaseVersion: string
 }
 
 export = scriptUtils

build-scripts/scriptUtils.js
@@ -5,13 +5,14 @@
 
 const path = require('path')
 
-repoRoot = path.dirname(__dirname)
-tasksDirectory = 'Tasks'
-sourceTasks = path.join(repoRoot, tasksDirectory)
-buildRoot = path.join(repoRoot, '_build')
-buildTasks = path.join(buildRoot, tasksDirectory)
-packageRoot = path.join(repoRoot, '_package')
-packageTasks = path.join(packageRoot, tasksDirectory)
+const repoRoot = path.dirname(__dirname)
+const tasksDirectory = 'Tasks'
+const sourceTasks = path.join(repoRoot, tasksDirectory)
+const buildRoot = path.join(repoRoot, '_build')
+const buildTasks = path.join(buildRoot, tasksDirectory)
+const packageRoot = path.join(repoRoot, '_package')
+const packageTasks = path.join(packageRoot, tasksDirectory)
+const releaseVersion = require(path.join(repoRoot, 'package.json')).version
 
 module.exports = {
     repoRoot: repoRoot,
@@ -19,5 +20,6 @@ module.exports = {
     buildRoot: buildRoot,
     buildTasks: buildTasks,
     packageRoot: packageRoot,
-    packageTasks: packageTasks
+    packageTasks: packageTasks,
+    releaseVersion: releaseVersion
 }

commit 06ac4612f45825b1dce3e510a77da61c5a09a531

Fixing issues caused by #310
Fix empty version for tasks
Fix Cannot read property 'retrieveSecret' of undefined when running tasks

build-scripts/generateResources.js
@@ -9,6 +9,7 @@ const path = require('path')
 const syncRequest = require('sync-request')
 const validate = require('validator')
 const folders = require('./scriptUtils')
+const semverParse = require('semver/functions/parse')
 
 const timeMessage = 'Generated resources'
 const taskJson = 'task.json'
@@ -166,10 +167,11 @@ var createResjson = function(task, taskPath) {
 }
 
 function addVersionToTask(task, versionInfo) {
+    versionInfo = semverParse(versionInfo)
     task.version = {
-        Major: versionInfo.Major,
-        Minor: versionInfo.Minor,
-        Patch: versionInfo.Patch
+        Major: versionInfo.major,
+        Minor: versionInfo.minor,
+        Patch: versionInfo.patch
     }
 }
 

build-scripts/packageExtension.js
@@ -115,7 +115,7 @@ function package(options) {
         }
 
         shell.cd(taskPackageFolder)
-        var npmCmd = 'npm install vsts-task-lib --only=production'
+        var npmCmd = 'npm install azure-pipelines-task-lib --only=production'
         try {
             output = ncp.execSync(npmCmd, { stdio: 'pipe' })
             console.log(output)

webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
         }
     },
     externals: {
-        'vsts-task-lib/task': 'require("vsts-task-lib/task")'
+        'azure-pipelines-task-lib/task': 'require("azure-pipelines-task-lib/task")'
     },
     optimization: {
         minimizer: [new TerserPlugin({ cache: true, parallel: true, sourceMap: true })]