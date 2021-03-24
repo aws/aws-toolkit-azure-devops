0f799a965d7aa8fd842e995ef58e67710b57de27
Hunter Werlla <werlla@amazon.com>
2021-03-24 10:03:20 -0700

    Make it work properly

--- src/lib/sdkutils.ts
+++ src/lib/sdkutils.ts
@@ -157,6 +157,9 @@ export abstract class SdkUtils {
 
         const credentials = await getCredentials(taskParams)
 
+        // resolve credentials
+        await credentials?.getPromise()
+
         // Mask credentials from logs if they are accidentially printed somehow
         if (credentials?.accessKeyId) {
             tl.setSecret(credentials?.accessKeyId)
