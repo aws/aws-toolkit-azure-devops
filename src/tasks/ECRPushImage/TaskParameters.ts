d3cd399aa11cf5bb8ad7199bec8ed5f2f809556f
Maxim Hayes <hayemaxi@amazon.com>
2024-10-07 13:23:31 -0700

    feat(ecr): support retagging in push task
    See https://docs.aws.amazon.com/AmazonECR/latest/userguide/image-retag.html
    
    Adds an additional option to the ECR push task to specify an existing image in ECR by repository and tag, and then add a new tag to this image without re-pushing the complete image.
    It will fail if there is no corresponding tag + repo in ECR. It will NOT fail if the target image already has the tag being added.
    
    Doc changes pending.

--- src/tasks/ECRPushImage/TaskParameters.ts
+++ src/tasks/ECRPushImage/TaskParameters.ts
@@ -9,6 +9,7 @@ import { getInputOrEmpty, getInputRequired } from 'lib/vstsUtils'
 
 export const imageNameSource = 'imagename'
 export const imageIdSource = 'imageid'
+export const retagSource = 'retag'
 
 export interface TaskParameters {
     awsConnectionParameters: AWSConnectionParameters
@@ -17,7 +18,8 @@ export interface TaskParameters {
     sourceImageTag: string
     sourceImageId: string
     repositoryName: string
-    pushTag: string
+    targetTag: string
+    newTag: string
     autoCreateRepository: boolean
     forceDockerNamingConventions: boolean
     removeDockerImage: boolean
@@ -29,24 +31,37 @@ export function buildTaskParameters(): TaskParameters {
         awsConnectionParameters: buildConnectionParameters(),
         imageSource: getInputRequired('imageSource'),
         repositoryName: getInputRequired('repositoryName'),
-        pushTag: getInputOrEmpty('pushTag'),
+        targetTag: getInputOrEmpty('targetTag'),
         autoCreateRepository: tl.getBoolInput('autoCreateRepository', false),
         forceDockerNamingConventions: tl.getBoolInput('forceDockerNamingConventions', false),
         removeDockerImage: tl.getBoolInput('removeDockerImage', false),
         outputVariable: getInputOrEmpty('outputVariable'),
         sourceImageName: '',
         sourceImageId: '',
-        sourceImageTag: ''
+        sourceImageTag: '',
+        newTag: ''
     }
 
-    if (parameters.imageSource === imageNameSource) {
-        parameters.sourceImageName = getInputRequired('sourceImageName')
-        parameters.sourceImageTag = getInputOrEmpty('sourceImageTag')
-        if (!parameters.sourceImageTag) {
-            parameters.sourceImageTag = 'latest'
-        }
-    } else {
-        parameters.sourceImageId = getInputRequired('sourceImageId')
+    switch (parameters.imageSource) {
+        case imageNameSource:
+            parameters.sourceImageName = getInputRequired('sourceImageName')
+            parameters.sourceImageTag = getInputOrEmpty('sourceImageTag')
+            if (!parameters.sourceImageTag) {
+                parameters.sourceImageTag = 'latest'
+            }
+            break
+        case imageIdSource:
+            parameters.sourceImageId = getInputRequired('sourceImageId')
+            break
+        case retagSource:
+            parameters.newTag = getInputRequired('newTag')
+            break
+        default:
+            throw new Error(`Unknown imageSource specified: ${parameters.imageSource}`)
+    }
+
+    if (!parameters.targetTag) {
+        parameters.targetTag = 'latest'
     }
 
     return parameters
